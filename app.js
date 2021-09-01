614e40cbfa3b157b30ffff56bb72fee464b97b84
Flavio <flavioeco@yahoo.com.br>
2021-09-01 14:30:16 -0300

    testing pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!!! \n Testing AWS'));
+app.get('/', (req, res) => res.send('Hello World!!! \n Testing AWS \n Testing pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
